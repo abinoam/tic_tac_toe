111bfc13e330f10f2cb17e79c9fb77678db451d4
Abinoam Jr <abinoam@gmail.com>
2013-09-24 00:10:40 -0300

    Initial commit - Creating the board

---
+++ ttt.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env
+
+# TTT is TicTacToe Game
+
+# First thing we need is a board
+class TTTBoard
+  def initialize
+    @board = [[nil, nil, nil],
+              [nil, nil, nil],
+              [nil, nil, nil]]
+  end
+end
+
+              
+
