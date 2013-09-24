f63c18f2ae7fe8855a20c9d057314160c468a66d
Abinoam Jr <abinoam@gmail.com>
2013-09-24 03:52:52 -0300

    Add game play functionality

--- ttt.rb
+++ ttt.rb
@@ -101,3 +101,59 @@ class TTTBoard
     raise ArgumentError, "value should be 'X' or 'O' or nil but it is #{value}" unless ['X', 'O', nil].include?(value)
   end
 end
+
+class TTTGame
+  def initialize
+    @board = TTTBoard.new
+  end
+
+  def game_play
+    5.times do
+      board_display
+      player_choice
+      computer_choice
+      winner = @board.game_winner?
+      if winner
+        board_display
+        puts "The winner is #{winner}"
+        return winner
+      end
+    end
+    board_display
+    puts "The game was a draw"
+  end
+
+  def board_display
+    puts @board
+  end
+
+  def player_choice
+    loop do
+      puts "Please enter the row number (1,2, or 3): "
+      row = gets.chomp.to_i
+      puts "Please enter the col number (1,2, or 3): "
+      col = gets.chomp.to_i
+      if @board[row,col]
+        puts "Cell is full already"
+      else
+        @board[row,col] = "X"
+        break
+      end
+    end
+  rescue ArgumentError
+    puts "Values should be 1, 2 or 3"
+    player_choice
+  end
+
+  def computer_choice
+    # Pretty dumb computer oponent
+    # Just plays radomly
+    free_slots = @board.each_line.to_a.flatten.map.with_index {|e, ix| e.nil? ? ix : nil }.compact
+    return "No Free Slots" if free_slots.empty?
+    rand_pos = rand(free_slots.size)
+    free_slot = free_slots[rand_pos]
+    col = free_slot % 3 + 1
+    lin = free_slot / 3 + 1
+    @board[lin, col] = "O"
+  end
+end
