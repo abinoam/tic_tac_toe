559cd2d4d3f4b7a56340051bb0ea1a1735450e0d
Abinoam Jr <abinoam@gmail.com>
2013-09-24 00:19:30 -0300

    Add a way to access the board within its limits

--- ttt.rb
+++ ttt.rb
@@ -9,7 +9,9 @@ class TTTBoard
               [nil, nil, nil],
               [nil, nil, nil]]
   end
+
+  def [](x,y)
+    raise ArgumentError, "x should fall under the 1-3 limit but it is #{x}" unless (1..3).include?(x)
+    raise ArgumentError, "y should fall under the 1-3 limit but it is #{y}" unless (1..3).include?(y)
+  end
 end
-
-              
-
