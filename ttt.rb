09fa194b4af53e03f8b34228f784ae08b8155664
Abinoam Jr <abinoam@gmail.com>
2013-09-24 00:29:27 -0300

    Add a way to set and retrieve the board elements

--- ttt.rb
+++ ttt.rb
@@ -13,5 +13,13 @@ class TTTBoard
   def [](x,y)
     raise ArgumentError, "x should fall under the 1-3 limit but it is #{x}" unless (1..3).include?(x)
     raise ArgumentError, "y should fall under the 1-3 limit but it is #{y}" unless (1..3).include?(y)
+    @board[x-1][y-1]
+  end
+
+  def []=(x,y, value)
+    raise ArgumentError, "x should fall under the 1-3 limit but it is #{x}" unless (1..3).include?(x)
+    raise ArgumentError, "y should fall under the 1-3 limit but it is #{y}" unless (1..3).include?(y)
+    raise ArgumentError, "value should be 'X' or 'O' or nil but it is #{value}" unless ['X', 'O', nil].include?(value)
+    @board[x-1][y-1]=value
   end
 end
